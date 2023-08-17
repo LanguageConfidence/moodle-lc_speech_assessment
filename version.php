4d84175007389cd2106155ab75b852ee717eb574
Duy Nguyen <duy_nguyen@singlife.com>
2023-08-17 15:04:53 +1000

    update version

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023063010;
+$plugin->version   = 2023101710;
 $plugin->requires  = 2020061500;
 $plugin->component = 'qtype_lcspeech';
 $plugin->maturity  = MATURITY_STABLE;
